1493337e94293efc07b82c1531a17980477e6744
Johnny Kung <kjohnathan.engi@gmail.com>
2022-02-07 21:24:19 -0800

    change logic code to fail the test

--- Temperature/Conversion.cs
+++ Temperature/Conversion.cs
@@ -35,7 +35,7 @@ namespace Temperature
                     break;
                 case ConversionMode.Fahrenheit_to_Kelvin:
                     // K = 5/9 (° F - 32) + 273
-                    result = 5d/9d *(temperature - 32) +273;
+                    result = 5d/9d *(temperature - 30) +273;
                     break;
             }
             return Math.Round(result, 2);
